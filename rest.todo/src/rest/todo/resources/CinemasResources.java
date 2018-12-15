e2342ff8fb4f6b064bc0442713d0ec58e40ea30c
guillu97 <guillaume.farge@efrei.net>
2018-12-15 03:00:53 +0100

    added search by city

--- rest.todo/src/rest/todo/resources/CinemasResources.java
+++ rest.todo/src/rest/todo/resources/CinemasResources.java
@@ -1,12 +1,18 @@
 package rest.todo.resources;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
@@ -27,13 +33,16 @@ public class CinemasResources {
 	  Request request;
 	  
 	// Return the list of todos to the user in the browser
+	  
 	  @GET
 	  @Produces(MediaType.TEXT_XML)
+	  @Path("/all")
 	  public List<Cinema> getCinemasBrowser() {
 	    List<Cinema> cinemas = new ArrayList<Cinema>();
 	    cinemas.addAll(TodoDao.instance.getCinemas().values());
 	    return cinemas; 
 	  }
+	  
 	  
 	// Defines that the next path parameter after todos is
 	  // treated as a parameter and passed to the TodoResources
@@ -41,10 +50,12 @@ public class CinemasResources {
 	  // 1 will be treaded as parameter todo and passed to TodoResource
 	  @GET
 	  @Produces(MediaType.TEXT_XML)
-	  @Path("{city}/")
-	  public List<Cinema> getCinemasFrom(@PathParam("city") String city) {
+	  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	 
+	  public List<Cinema> getCinemasFrom(@QueryParam("city") String city) {
 		  ArrayList<Cinema> list = new ArrayList<Cinema>();
-			
+		  	//debug:
+			//System.out.println("city:"+city);
 			for(Cinema cinema : TodoDao.instance.getCinemas().values()) {
 				if(cinema==null)
 					throw new RuntimeException("Get: Cinema with " + city +  " not found");
