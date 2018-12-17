6f531a6a5d61fc25407b41a34ebb6d73ea2ef94f
ClaireH97 <Claire.huang@efrei.net>
2018-12-18 00:52:52 +0100

    Comment line + some changes

--- rest.todo/src/rest/todo/resources/MoviesResources.java
+++ rest.todo/src/rest/todo/resources/MoviesResources.java
@@ -1,5 +1,10 @@
 package rest.todo.resources;
 
+import rest.todo.dao.TodoDao;
+import rest.todo.model.ListMovies;
+import rest.todo.model.Movie;
+import rest.todo.model.Session;
+
 import java.io.IOException;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -9,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -22,200 +26,196 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
-import rest.todo.dao.TodoDao;
-import rest.todo.model.Cinema;
-import rest.todo.model.ListMovies;
-import rest.todo.model.Movie;
-import rest.todo.model.Session;
-
+//Will map the resource to the URL movies
 @Path("/movies")
 public class MoviesResources {
-	// Allows to insert contextual objects into the class, 
-	  // e.g. ServletContext, Request, Response, UriInfo
-	  @Context
-	  UriInfo uriInfo;
-	  @Context
-	  Request request;
-	  
-	  @GET
-	  @Produces(MediaType.TEXT_HTML)
-	  public String getMoviesHTML() {
-	    ListMovies movies = new ListMovies();
-	    movies.addAll(TodoDao.instance.getMovies().values());
-	    return "" + movies; 
-	  }
-	  
-	  @GET
-	  @Produces(MediaType.TEXT_HTML)
-	  @Path("{id}")
-	  public String getMovie(@PathParam("id") int id) {
-		  Movie movie = TodoDao.instance.getMovies().get(id);
-		  
-		  // if the there is no movie with the id entered
-		  if(movie == null)
-			  return "<html>The movie with this id does not exist</html>";
-		  
-		  // the way to display the sessions
-		  ArrayList<Session> sessionList = new ArrayList<Session>() {
-				@Override
-				// TODO 
-				public
-				String toString() {
-					int count = 1;
-					String str = "";
-					for(Session session : this) {
-						str+="<h3>Session " + count + "</h3>";
-						str+= TodoDao.instance.getCinemas().get(session.getCinema_id());
-						str+="<br>Date: ";
-						str+=session.getDate();
-						str+="<br>Hour:";
-						str+=session.getHour();
-						str+="<br>Version:";
-						str+=session.getVersion();
-						count++;
-					}
-									
-					return str;
-				}
-			};
-			
-			// search for the session with the movie id
-		  for(Session session : TodoDao.instance.getSessions().values() ) {
-			  if(session.getMovie_id() == movie.getId()) {
-				  sessionList.add(session);
-			  }
-		  }
-		  
-		  if(movie != null)
-			  return "<html>" + movie.allTheInfos() + "<h2>" + "All the sessions for this movie</h2>" +  sessionList +  "</html>";
-		  else
-			  return "<html>The movie with this id" + movie.getId() + " doesn\'t exist</html>";
-	  }
-	  
-	  @Path("/dropdown")
-	  @GET
-	  @Produces(MediaType.TEXT_HTML)
-	  public String getCinemasDropdown() {
-	    List<Movie> movies = new ArrayList<Movie>(){
+	// Allows to insert contextual objects into the class,
+	// e.g. ServletContext, Request, Response, UriInfo
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
+
+	
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/movies 
+	 * Return all the movies in HTML Text
+	 */
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getMoviesHTML() {
+		ListMovies movies = new ListMovies();
+		movies.addAll(TodoDao.instance.getMovies().values());
+		return "" + movies;
+	}
+
+	
+    /*
+     * Use http://localhost:8080/rest.todo/rest/movies/{id}
+     * Return the informations of a movie into <html>
+     */
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Path("{id}")
+	public String getMovie(@PathParam("id") int id) {
+		Movie movie = TodoDao.instance.getMovies().get(id);
+
+		// if the there is no movie with the id entered
+		if (movie == null)
+			return "<html>The movie with this id does not exist</html>";
+
+		// the way to display the sessions
+		ArrayList<Session> sessionList = new ArrayList<Session>() {
 			@Override
-			public
-			String toString() {
-				String str = "<select name=\"movie_id\">";
-				for(Movie movie : this) {
-					str+="<option value=\"" + movie.getId() + "\">";
-					str+=movie.getName();
-					str+="</option>";
+			// TODO
+			public String toString() {
+				int count = 1;
+				String str = "";
+				for (Session session : this) {
+					str += "<h3>Session " + count + "</h3>";
+					str += TodoDao.instance.getCinemas().get(session.getCinema_id());
+					str += "<br>Date: ";
+					str += session.getDate();
+					str += "<br>Hour:";
+					str += session.getHour();
+					str += "<br>Version:";
+					str += session.getVersion();
+					count++;
 				}
-				str+="</select>";
-								
 				return str;
 			}
 		};
-	    movies.addAll(TodoDao.instance.getMovies().values());
-	    return "" + movies; 
-	  }
-	  
-	  
-	  // Defines that the next path parameter after todos is
-	  // treated as a parameter and passed to the TodoResources
-	  // Allows to type http://localhost:8080/rest.todo/rest/todos/1
-	  // 1 will be treaded as parameter todo and passed to TodoResource
-	  @GET
-	  @Path("/query")
-	  @Produces(MediaType.TEXT_HTML)
-	  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	  public String getCinemasFrom(@QueryParam("name") String name) {
-		  ListMovies list = new ListMovies();
-		  	//debug:
-//				//System.out.println("city:"+city);
-			
-			for(Movie movie : TodoDao.instance.getMovies().values()) {
-				if(movie==null)
-					throw new RuntimeException("Get: Movie with " + name +  " not found");
-				if(movie.getName().contains(name) ) {
-					list.add(movie);
-				}
+
+		// search for the session with the movie id
+		for (Session session : TodoDao.instance.getSessions().values()) {
+			if (session.getMovie_id() == movie.getId()) {
+				sessionList.add(session);
 			}
-			
-	    return "<html>" + list + "</html>";
-	  }
-	  
-	  
-	  // retuns the number of todos
-	  // Use http://localhost:8080/rest.todo/rest/todos/count
-	  // to get the total number of records
-	  @GET
-	  @Path("count")
-	  @Produces(MediaType.TEXT_PLAIN)
-	  public String getCount() {
-	    int count = TodoDao.instance.getMovies().size();
-	    return String.valueOf(count);
-	  }
-	  
-	  
-	  @POST
-	  @Path("/createMovie")
-	  @Produces(MediaType.TEXT_HTML)
-	  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	  public void newCinema(@FormParam("name") String name,
-			  @FormParam("rate") String rate,
-			  @FormParam("type") String type,
-			  @FormParam("actor") String actor,
-			  @FormParam("producer") String producer,
-			  @FormParam("length") String length,
-			  @FormParam("country") String country,
-			  @FormParam("on_screen_date") Date on_screen_date,
-			  @FormParam("language") String language,
-			  @FormParam("description") String description,
-	      @Context HttpServletResponse servletResponse,
-	      @Context HttpServletRequest request) throws IOException, ServletException {
-		  System.out.println("name : " + name);
-		  System.out.println("rate : " + rate);
-		  System.out.println("type : " + type);
-		  System.out.println("actor : " + actor);
-		  System.out.println("producer : " + producer);
-		  System.out.println("length : " + length);
-		  System.out.println("country : " + country);
-		  System.out.println("on_screen_date" + on_screen_date);
-		  System.out.println("language : " + language);
-		  System.out.println("description : " + description);
-		  
-	    Movie movie = new Movie(name, Integer.parseInt(rate), type,  actor, producer, Integer.parseInt(length), country, on_screen_date, language, description, "test");
-	    TodoDao.instance.getMovies().put(movie.getId(),movie);
-	    
-	    // TODO when changes.
-	    request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse); 
-	    //servletResponse.sendRedirect("../../login.html");
-	  }	  
-	  
-	  
-	  @POST
-	  @Path("/deleteMovie")
-	  @Produces(MediaType.TEXT_HTML)
-	  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	  public void newCinema(@FormParam("movie_id") int movieId,
-	      @Context HttpServletResponse servletResponse,
-	      @Context HttpServletRequest request) throws IOException, ServletException {
-		  System.out.println("movie_id : " + movieId);
-	   
-	    
-		for(Session session : TodoDao.instance.getSessions().values()) {
-			if(session==null)
-				throw new RuntimeException("Get: session with " + movieId +  " not found");
-			if(session.getMovie_id() == (movieId) ) {
+		}
+
+		if (movie != null)
+			return "<html>" + movie.allTheInfos() + "<h2>" + "All the sessions for this movie</h2>" + sessionList
+					+ "</html>";
+		else
+			return "<html>The movie with this id" + movie.getId() + " doesn\'t exist</html>";
+	}
+
+	
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/movies/dropdown
+	 * Return all the movies existing into a dropdown box
+	 */
+	@Path("/dropdown")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getMoviesDropdown() {
+		List<Movie> movies = new ArrayList<Movie>() {
+			@Override
+			public String toString() {
+				String str = "<select name=\"movie_id\">";
+				for (Movie movie : this) {
+					str += "<option value=\"" + movie.getId() + "\">";
+					str += movie.getName();
+					str += "</option>";
+				}
+				str += "</select>";
+				return str;
+			}
+		};
+		movies.addAll(TodoDao.instance.getMovies().values());
+		return "" + movies;
+	}
+
+	
+	/*
+	 * For the searching part Use http://localhost:8080/rest.todo/rest/movies/query
+	 * return the list of movies containing the query "name" entered as a Param
+	 */
+	@GET
+	@Path("/query")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getMoviesFrom(@QueryParam("name") String name) {
+		ListMovies list = new ListMovies();
+		for (Movie movie : TodoDao.instance.getMovies().values()) {
+			if (movie == null)
+				throw new RuntimeException("Get: Movie with " + name + " not found");
+			if (movie.getName().contains(name)) {
+				list.add(movie);
+			}
+		}
+		return "<html>" + list + "</html>";
+	}
+
+	
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/movies/count 
+	 * return the total number of movies
+	 */
+	@GET
+	@Path("count")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCount() {
+		int count = TodoDao.instance.getMovies().size();
+		return String.valueOf(count);
+	}
+
+	
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/movies/createMovie 
+	 * Allows us the creation of a movie by getting informations from a form
+	 */
+	@POST
+	@Path("/createMovie")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void newMovie(@FormParam("name") String name, @FormParam("rate") String rate,
+			@FormParam("type") String type, @FormParam("actor") String actor, @FormParam("producer") String producer,
+			@FormParam("length") String length, @FormParam("country") String country,
+			@FormParam("on_screen_date") Date on_screen_date, @FormParam("language") String language,
+			@FormParam("description") String description, @Context HttpServletResponse servletResponse,
+			@Context HttpServletRequest request) throws IOException, ServletException {
+		System.out.println("name : " + name);
+		System.out.println("rate : " + rate);
+		System.out.println("type : " + type);
+		System.out.println("actor : " + actor);
+		System.out.println("producer : " + producer);
+		System.out.println("length : " + length);
+		System.out.println("country : " + country);
+		System.out.println("on_screen_date" + on_screen_date);
+		System.out.println("language : " + language);
+		System.out.println("description : " + description);
+
+		Movie movie = new Movie(name, Integer.parseInt(rate), type, actor, producer, Integer.parseInt(length), country,
+				on_screen_date, language, description, "test");
+		TodoDao.instance.getMovies().put(movie.getId(), movie);
+
+		request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse);
+	}
+
+	
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/movies/deleteMovie
+	 * Delete the movie selected by the user into a form
+	 */
+	@POST
+	@Path("/deleteMovie")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void newCinema(@FormParam("movie_id") int movieId, @Context HttpServletResponse servletResponse,
+			@Context HttpServletRequest request) throws IOException, ServletException {
+		System.out.println("movie_id : " + movieId);
+
+		for (Session session : TodoDao.instance.getSessions().values()) {
+			if (session == null)
+				throw new RuntimeException("Get: session with " + movieId + " not found");
+			if (session.getMovie_id() == (movieId)) {
 				TodoDao.instance.getMovies().remove(session);
 			}
 		}
-		
-		
-		 TodoDao.instance.getMovies().remove(movieId);
-	    // TODO when changes.
-	    request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse); 
-	    //servletResponse.sendRedirect("../../create_session.html");
-	    
-	    
-	  	//debug:
-//			//System.out.println("city:"+city);
-		
-	  }
+
+		TodoDao.instance.getMovies().remove(movieId);
+
+		request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse);
+	}
 }
--- rest.todo/src/rest/todo/resources/SessionsResources.java
+++ rest.todo/src/rest/todo/resources/SessionsResources.java
@@ -34,173 +34,163 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
-
 //Will map the resource to the URL sessions
 @Path("/sessions")
 public class SessionsResources {
-	  // Allows to insert contextual objects into the class, 
-	  // e.g. ServletContext, Request, Response, UriInfo
-	  @Context
-	  UriInfo uriInfo;
-	  @Context
-	  Request request;
-	  
-	  /*
-	   * Use http://localhost:8080/rest.todo/rest/sessions/{id}
-	   * Return the informations of session into <html>
-	   */
-	  @GET
-	  @Path("{id}")
-	  @Produces(MediaType.TEXT_HTML)
-	  public String getSession(@PathParam("id") int id) {
-		  Session session = TodoDao.instance.getSessions().get(id);
-		  if(session != null)
-			  return "<html>" + session + "</html>";
-		  else
-			  return "<html>The session with this id" + session.getId() +" doesn\'t exist</html>";
-	  }
-	  
+	// Allows to insert contextual objects into the class,
+	// e.g. ServletContext, Request, Response, UriInfo
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
 
-	  /*
-	   * Use http://localhost:8080/rest.todo/rest/sessions/dropdown
-	   * Return all the sessions existing into a dropdown box
-	   */
-	  @GET
-	  @Path("/dropdown")
-	  @Produces(MediaType.TEXT_HTML)
-	  public String getSessionsDropdown() {
-	    List<Session> sessions = new ArrayList<Session>(){
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/sessions/{id} 
+	 * Return the informations of session into <html>
+	 */
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.TEXT_HTML)
+	public String getSession(@PathParam("id") int id) {
+		Session session = TodoDao.instance.getSessions().get(id);
+		if (session != null)
+			return "<html>" + session + "</html>";
+		else
+			return "<html>The session with this id" + session.getId() + " doesn\'t exist</html>";
+	}
+
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/sessions/dropdown 
+	 * Return all the sessions existing into a dropdown box
+	 */
+	@GET
+	@Path("/dropdown")
+	@Produces(MediaType.TEXT_HTML)
+	public String getSessionsDropdown() {
+		List<Session> sessions = new ArrayList<Session>() {
 			@Override
 			public String toString() {
 				String str = "<select name=\"session_id\">";
-				for(Session sessions : this) {
+				for (Session sessions : this) {
 					Cinema cinema = TodoDao.instance.getCinemas().get(sessions.getCinema_id());
 					Movie movie = TodoDao.instance.getMovies().get(sessions.getMovie_id());
-					str+="<option value=\"" + sessions.getId() + "\">";
-					str+=cinema.getName() + " ";
-					str+=movie.getName() + " ";
-					str+=sessions.getHour() + " ";
-					str+=sessions.getDate() + " ";
-					str+="</option>";
+					str += "<option value=\"" + sessions.getId() + "\">";
+					str += cinema.getName() + " ";
+					str += movie.getName() + " ";
+					str += sessions.getHour() + " ";
+					str += sessions.getDate() + " ";
+					str += "</option>";
 				}
-				str+="</select>";				
+				str += "</select>";
 				return str;
 			}
 		};
 		// added all the sessions into the List<Sessions> sessions
 		sessions.addAll(TodoDao.instance.getSessions().values());
-	    return "" + sessions; 
-	  }
-	  
-	  
-	  /*
-	   * Use http://localhost:8080/rest.todo/rest/sessions
-	   * Return all the sessions in HTML Text
-	   */
-	  @GET
-	  @Produces(MediaType.TEXT_HTML)
-	  public String getSessionHTML() {
-	    List<Session> sessions = new ArrayList<Session>(){
+		return "" + sessions;
+	}
+
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/sessions 
+	 * Return all the sessions in HTML Text
+	 */
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getSessionHTML() {
+		List<Session> sessions = new ArrayList<Session>() {
 			@Override
 			public String toString() {
 				String str = "";
-				for(Session session : this) {
-					str+=session;
+				for (Session session : this) {
+					str += session;
 				}
 				return str;
 			}
 		};
-	    sessions.addAll(TodoDao.instance.getSessions().values());
-	    return "<html>" + sessions + "</html>"; 
-	  }
-	  
-	  
-	  /* 
-	   * Use http://localhost:8080/rest.todo/rest/sessions/count
-	   * return the total number of sessions
-	   */
-	  @GET
-	  @Path("count")
-	  @Produces(MediaType.TEXT_PLAIN)
-	  public String getCount() {
-	    int count = TodoDao.instance.getSessions().size();
-	    return String.valueOf(count);
-	  }
-	  
-	  
-	  /* 
-	   * Use http://localhost:8080/rest.todo/rest/sessions/createSession
-	   * Allows us the creation of a session by getting information from a form
-	   */
-	  @POST
-	  @Path("/createSession")
-	  @Produces(MediaType.TEXT_HTML)
-	  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	  public void newSession(  @FormParam("movie_id") int movieId,
-							  @FormParam("cinema_id") int cinemaId,
-							  @FormParam("hour") Time hour,
-							  @FormParam("date") Date date,
-							  @FormParam("version") String version,
-						      @Context HttpServletResponse servletResponse,
-						      @Context HttpServletRequest request) throws IOException, ServletException {
-		  System.out.println("movie_id : " + movieId);
-		  System.out.println("cinema_id : " + cinemaId);
-		  System.out.println("hour : " + hour);
-		  System.out.println("date : " + date);
-		  System.out.println("version : " + version);
-	    Session session = new Session(movieId,cinemaId,hour,date,version);
-	    TodoDao.instance.getSessions().put(session.getId(),session);
-	    
-	    // TODO when changes.
-	    request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse); 
-	    //servletResponse.sendRedirect("../../create_session.html");
-	  }
-	  
+		sessions.addAll(TodoDao.instance.getSessions().values());
+		return "<html>" + sessions + "</html>";
+	}
 
-	  /* 
-	   * Use http://localhost:8080/rest.todo/rest/sessions/deleteSession
-	   * return the total number of sessions
-	   */
-	  @POST
-	  @Path("/deleteSession")
-	  @Produces(MediaType.TEXT_HTML)
-	  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	  public void removeSession(  @FormParam("session_id") int sessionId,
-							      @Context HttpServletResponse servletResponse,
-							      @Context HttpServletRequest request) throws IOException, ServletException {
-		    System.out.println("sessionId : " + sessionId);
-	   
-			//collect data from Database
-			Connection con;
-			String driver = Constants.driver;
-			String url = Constants.url;
-			String user = Constants.user;
-			String password = Constants.password;
-			
-			//begin to delete
-			try {
-				 Class.forName(driver);
-				 con = DriverManager.getConnection(url,user,password);
-				 PreparedStatement ps=con.prepareStatement("delete from session where id = ?");
-				  ps.setInt(1,sessionId);
-				  ps.executeUpdate();
-				  ps.close();
-				  con.close();
-			
-		    //driver Exception & connection Exception
-			}catch(ClassNotFoundException e) {
-				System.out.println("Sorry,can`t find the Driver!"); 	
-				e.printStackTrace(); 
-			}catch(SQLException e) {
-				 e.printStackTrace();  
-			}catch (Exception e) {
-				 e.printStackTrace();
-			}finally{
-				//System.out.println("Success access to Database£¡£¡");
-			}
-		  
-	    request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse); 
-	    //servletResponse.sendRedirect("../../create_session.html");
-	  }
-	  
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/sessions/count return the total
+	 * number of sessions
+	 */
+	@GET
+	@Path("count")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCount() {
+		int count = TodoDao.instance.getSessions().size();
+		return String.valueOf(count);
+	}
+
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/sessions/createSession 
+	 * Allows us the creation of a session by getting information from a form
+	 */
+	@POST
+	@Path("/createSession")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void newSession(@FormParam("movie_id") int movieId, @FormParam("cinema_id") int cinemaId,
+			@FormParam("hour") Time hour, @FormParam("date") Date date, @FormParam("version") String version,
+			@Context HttpServletResponse servletResponse, @Context HttpServletRequest request)
+			throws IOException, ServletException {
+		System.out.println("movie_id : " + movieId);
+		System.out.println("cinema_id : " + cinemaId);
+		System.out.println("hour : " + hour);
+		System.out.println("date : " + date);
+		System.out.println("version : " + version);
+		Session session = new Session(movieId, cinemaId, hour, date, version);
+		TodoDao.instance.getSessions().put(session.getId(), session);
+
+		// TODO when changes.
+		request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse);
+		// servletResponse.sendRedirect("../../create_session.html");
+	}
+
+	/*
+	 * Use http://localhost:8080/rest.todo/rest/sessions/deleteSession 
+	 * Delete the session selected by the user into a form
+	 */
+	@POST
+	@Path("/deleteSession")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void removeSession(@FormParam("session_id") int sessionId, @Context HttpServletResponse servletResponse,
+			@Context HttpServletRequest request) throws IOException, ServletException {
+		System.out.println("sessionId : " + sessionId);
+
+		// collect data from Database
+		Connection con;
+		String driver = Constants.driver;
+		String url = Constants.url;
+		String user = Constants.user;
+		String password = Constants.password;
+
+		// begin to delete
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			PreparedStatement ps = con.prepareStatement("delete from session where id = ?");
+			ps.setInt(1, sessionId);
+			ps.executeUpdate();
+			ps.close();
+			con.close();
+
+			// driver Exception & connection Exception
+		} catch (ClassNotFoundException e) {
+			System.out.println("Sorry,can`t find the Driver!");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// System.out.println("Success access to Database£¡£¡");
+		}
+
+		request.getRequestDispatcher("/WEB-INF/administration.html").forward(request, servletResponse);
+		// servletResponse.sendRedirect("../../create_session.html");
+	}
+
 }
